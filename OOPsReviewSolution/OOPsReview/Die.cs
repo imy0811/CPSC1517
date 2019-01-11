2a2a920e7e10233ab20c6c5aa2d16f2c855ac047
imy0811 <43006948+imy0811@users.noreply.github.com>
2019-01-11 16:16:41 -0700

    update

--- OOPsReviewSolution/OOPsReview/Die.cs
+++ OOPsReviewSolution/OOPsReview/Die.cs
@@ -17,7 +17,7 @@ namespace OOPsReview
         //Private data members cannot be reached directly by the user
         //Public data members can be reached directly by the users
         private int _Size;
-
+        private string _Color;
         //Properties
         //is an external interface between the users and a single piece of data within the instance
         //A property has two abilities
@@ -29,7 +29,7 @@ namespace OOPsReview
         {
             get
             {
-                //Takes internal values and returns it to the user
+                //Takes internal value and returns it to the user
                 return _Size;
             }
 
@@ -37,9 +37,50 @@ namespace OOPsReview
             {
                 //Takes the supplied user value and places it into the internal private data member
                 //The incoming piece of data is placed into a special variable that is called: value
-                _Size = value;
+                //Optionally you may place validation on the incoming value
+                if (value >= 6 && value <= 20)
+                {
+                    _Size = value;
+                }
+                else
+                {
+                    throw new Exception("Die cannot be " + value.ToString() + " sides. Die must have between 6 and 20 sides.");
+                }                
             }
         }
+        //Auto Implemented Property
+        //  Public
+        //  It has a datatype
+        //  It has a name
+        //  It DOES NOT have an internal data member that you can directly access
+        //  The system will create, internally, a data storage area of the apropriate datatype and manage the area
+        //  The only way to access the data of an Auto Implemented Property is via the property
+        //  Usually use when there is no need for any internal validation or other proprty logic
+        public int FaceValue { get; set; }
+
+        public string Color
+        {
+            get
+            {
+                return _Color;
+            }
+            set
+            {
+                // (value == null) this will fail for an empty string
+                // (value ==)This will fail for a null value
+                //
+                if (string.IsNullOrEmpty(value))
+                {
+                    throw new Exception("Color has no value.");
+                }
+                else
+                {
+                    _Color = value;
+                }
+            }
+        }
+
+
         //Constructors
 
         //Behaviours (methods)
